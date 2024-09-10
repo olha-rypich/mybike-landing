75ca3061485bf81f690cc46739be5fcc001d7920
Olha Rypich <olha.rypich@gmail.com>
2024-09-10 13:36:37 +0300

    make build

---
+++ index.f69fb636.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector(".contacts__form");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.f69fb636.js.map
